cbe8a657bff53dbc46fd0bb318fadaed7c7858c1
Nicolas VERHELST <nicolas.nv.verhelst@gmail.com>
2021-07-10 17:37:54 +1000

    Initial commit: working with one key/button (key '4')

---
+++ Config/clock_config.h
@@ -0,0 +1,13 @@
+/* Auto-generated config file clock_config.h */
+#ifndef CLOCK_CONFIG_H
+#define CLOCK_CONFIG_H
+
+// <<< Use Configuration Wizard in Context Menu >>>
+
+#ifndef F_CPU
+#define F_CPU 16000000 // Low fuse C1 (No CLKDIV8, no CK out, super fast SUT, PLL: 16MHz)
+#endif
+
+// <<< end of configuration section >>>
+
+#endif // CLOCK_CONFIG_H
